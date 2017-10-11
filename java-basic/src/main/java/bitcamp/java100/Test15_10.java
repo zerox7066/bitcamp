ff21279713d253d2564b77e974c38d6d0ab31850
zerox7066 <zerox7066@gamil.com>
2017-10-11 10:28:24 +0900

    ..

--- java-basic/src/main/java/bitcamp/java100/Test15_10.java
+++ java-basic/src/main/java/bitcamp/java100/Test15_10.java
@@ -10,5 +10,14 @@ public class Test15_10 {
         
         int result = v1 & v2;
         System.out.println(java.lang.Integer.toBinaryString(result));
+        
+        result = v1 | v2;
+        System.out.println(java.lang.Integer.toBinaryString(result));
+        
+        result = v1 ^ v2;
+        System.out.println(java.lang.Integer.toBinaryString(result));
+        
+        result = ~v1;
+        System.out.println(java.lang.Integer.toBinaryString(result));
     }
 }
