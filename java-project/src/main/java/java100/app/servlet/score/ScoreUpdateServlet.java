9f3d6e82e63a7f494afac511fffbef21f446bc2e
dazzul94 <dazzul@naver.com>
2017-12-20 20:08:01 +0900

    '.'

--- java-project/src/main/java/java100/app/servlet/score/ScoreUpdateServlet.java
+++ java-project/src/main/java/java100/app/servlet/score/ScoreUpdateServlet.java
@@ -15,8 +15,8 @@ import java100.app.domain.Score;
 import java100.app.listener.ContextLoaderListener;
 
 @SuppressWarnings("serial")
-@WebServlet("/score/update1")   
-public class ScoreUpdateServlet extends HttpServlet {
+@WebServlet("/score/view1")   
+public class ScoreViewServlet extends HttpServlet {
     
     @Override
     public void service(HttpServletRequest request, HttpServletResponse response) 
@@ -26,7 +26,7 @@ public class ScoreUpdateServlet extends HttpServlet {
 
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        
+
         out.println("<!DOCTYPE html>");
         out.println("<html>");
         out.println("<head>");
@@ -40,28 +40,88 @@ public class ScoreUpdateServlet extends HttpServlet {
         RequestDispatcher rd = request.getRequestDispatcher("/header");
         rd.include(request, response);
         
-        out.println("<h1>성적 변경</h1>");
+        out.println("<h1>성적 상세 정보</h1>");
+        
         
         try {
-            Score score = new Score();
-            score.setNo(Integer.parseInt(request.getParameter("no")));
-            score.setName(request.getParameter("name"));
-            score.setKor(Integer.parseInt(request.getParameter("kor")));
-            score.setEng(Integer.parseInt(request.getParameter("eng")));
-            score.setMath(Integer.parseInt(request.getParameter("math")));
-            
-            if(scoreDao.update(score) > 0) {
-                out.println("<p>변경하였습니다.</p>");
+            int no = Integer.parseInt(request.getParameter("no"));
+            Score score = scoreDao.selectOne(no);
+                        
+            if (score != null) {
+                out.println("<form action='update' method='post'>");
+                
+                out.println("<div class='form-group row'>");
+                out.println("<label for='no' class='col-sm-2 col-form-label'>번호</label>");
+                out.println("<div class='col-sm-10'>");
+                out.printf("<input class='form-control' readonly id='no' type='number' name='no' value='%d'>\n",
+                        score.getNo());
+                out.println("</div>");
+                out.println("</div>");
+
+                out.println("<div class='form-group row'>");
+                out.println("<label for='name' class='col-sm-2 col-form-label'>이름</label>");
+                out.println("<div class='col-sm-10'>");
+                out.printf("<input class='form-control' id='name' type='text' name='name' value='%s'>\n",
+                        score.getName());                
+                out.println("</div>");
+                out.println("</div>");
+
+                out.println("<div class='form-group row'>");
+                out.println("<label for='kor' class='col-sm-2 col-form-label'>국어</label>");
+                out.println("<div class='col-sm-10'>");
+                out.printf("<input class='form-control' id='kor' type='number' name='kor' value='%d'>\n",
+                        score.getKor());                
+                out.println("</div>");
+                out.println("</div>");
+
+                out.println("<div class='form-group row'>");
+                out.println("<label for='eng' class='col-sm-2 col-form-label'>영어</label>");
+                out.println("<div class='col-sm-10'>");
+                out.printf("<input class='form-control' id='eng' type='number' name='eng' value='%d'>\n",
+                        score.getEng());                
+                out.println("</div>");
+                out.println("</div>");
+
+                out.println("<div class='form-group row'>");
+                out.println("<label for='math' class='col-sm-2 col-form-label'>수학</label>");
+                out.println("<div class='col-sm-10'>");
+                out.printf("<input class='form-control' id='math' type='number' name='math' value='%d'>\n",
+                        score.getMath());                
+                out.println("</div>");
+                out.println("</div>");
+
+                out.println("<div class='form-group row'>");
+                out.println("<label for='sum' class='col-sm-2 col-form-label'>합계</label>");
+                out.println("<div class='col-sm-10'>");
+                out.printf("<input class='form-control' readonly id='sum' type='text' value='%d'>\n",
+                        score.getSum());                
+                out.println("</div>");
+                out.println("</div>");
+
+                out.println("<div class='form-group row'>");
+                out.println("<label for='aver' class='col-sm-2 col-form-label'>평균</label>");
+                out.println("<div class='col-sm-10'>");
+                out.printf("<input class='form-control' readonly id='aver' type='text' value='%.1f'>\n",
+                        score.getAver());                
+                out.println("</div>");
+                out.println("</div>");
+                
+                out.println("<div class='form-group row'>");
+                out.println("<div class='col-sm-10'>");
+                out.println("<button class='btn btn-primary btn-sm'>변경</button>");
+                out.printf("<a href='delete?no=%d' class='btn btn-primary btn-sm'>삭제</a>\n", score.getNo());             
+                out.println("</div>");
+                out.println("</div>");
+                out.println("</form>");
+                
             } else {
-                out.printf("<p>'%s'의 성적 정보가 없습니다.</p>\n", score.getNo());
+                out.printf("'%d'의 성적 정보가 없습니다.\n", no); 
             }
             
         } catch (Exception e) {
             e.printStackTrace(); // for developer
             out.println(e.getMessage()); // for user
         }
-        
-        out.println("<p><a href='list' class='btn btn-primary btn-sm'>목록</a></p>");
         
         rd = request.getRequestDispatcher("/footer");
         rd.include(request, response);
@@ -75,7 +135,9 @@ public class ScoreUpdateServlet extends HttpServlet {
         out.println("</body>");
         out.println("</html>");
     }
+    
 }
+
 
 
 
